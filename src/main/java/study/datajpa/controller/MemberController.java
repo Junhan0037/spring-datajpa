59cf8dadf1e63e1ae0510d27b76992196cb65fae
Junhan0037 <wnsgks0037@naver.com>
2020-06-22 15:08:25 +0900

    페이징과 정렬 확장

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,6 +1,8 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,9 +28,22 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members") // http://localhost:8080/members?page=?&size=?&sort=id.desc
+    public Page<Member> list(Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable); // 엔티티를 외부에 노출해서는 안된다
+        return page;
+    }
+
+//    @PostConstruct
+//    public void init() {
+//        memberRepository.save(new Member("userA"));
+//    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 
 }
